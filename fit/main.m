eb5c964f6bf02f462a9eb987722bbccf24587c1c
devin <devinbalian@gmail.com>
2023-07-14 12:54:59 +0200

    fixed fresnel number calculation save ebow as file "ebow.mat" intensity calculationwithout PML boundary

--- fit/main.m
+++ fit/main.m
@@ -32,18 +32,19 @@ d = 20; % "Gitter-Abstand"
 lambda = 3e8/f ; % Wavelength;
 [ msh ] = cartMesh2D( linspace(1,x*lambda, x*elem_per_wavelength), linspace(1,y*lambda, y*elem_per_wavelength) );
 
-b = floor((x*elem_per_wavelength)/2);
+b = floor((x*elem_per_wavelength)/2) + msh.ny * (20 + elem_per_wavelength);
 jsbow = sparse(msh.np, 1);
 jsbow(floor(b - d*elem_per_wavelength ),1) = excitation;
 jsbow(ceil(b + d*elem_per_wavelength),1) = excitation;
 
-display(fresnel_number(y*elem_per_wavelength, max(diff(msh.xmesh)), max(diff(msh.ymesh)), f))
+display(fresnel_number(d, y*elem_per_wavelength, max(diff(msh.xmesh)), max(diff(msh.ymesh)), f))
 
 %% Solution
 eps = 8.854e-12;
 mui = 1/(4*pi*1e-7);
 
 ebow = solveHelmholtzTE(msh, eps, mui, jsbow, omega, 0);
+save('ebow.mat', 'ebow')
 ibov = reshape(real(ebow*exp(-1i*omega)),[msh.nx, msh.ny]);
 
 %% Postprocessing
@@ -54,12 +55,13 @@ set(h,'LineStyle','none')
 set(gca,'ColorScale','log')
 figure
 
-intensity = ibov(:,end);
+% TODO: proper intensity calculation
+intensity = ibov(20:end-20,end-20).^2;
 plot(1:length(intensity), abs(intensity))
 
 
-function fnum = fresnel_number(nL, dx, dy, f)
-    a = dx;
+function fnum = fresnel_number(nD ,nL, dx, dy, f)
+    a = nD * dx;
     L = nL * dy;
     fnum = a^2 * f / (L * 3e8);
     assert(fnum < .2);
