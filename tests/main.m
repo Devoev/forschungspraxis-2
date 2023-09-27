940c99b02d9ae7434a2b22d7351652c3c94538d1
Devin <devinbalian@gmail.com>
2023-09-27 13:39:59 +0200

    updated tests

---
+++ tests/main.m
@@ -0,0 +1,4 @@
+%% Run all tests here
+run('test_geo_mats_2d')
+
+disp('All tests passed successfully!')
--- tests/test_geo_mats_2d.m
+++ tests/test_geo_mats_2d.m
@@ -13,7 +13,8 @@ np = msh.np;
 [ds, dst, da, dat] = createGeoMats2D(msh);
 [dsTE, dstTE, daTE, datTE] = createGeoMatsTE(msh);
 
-normest(dsTE - ds(2*np+1:end, 2*np+1:end))
-normest(dst(1:2*np, 1:2*np) - dstTE)
-normest(da(1:2*np, 1:2*np) - daTE)
-normest(dat(2*np+1:end, 2*np+1:end) - datTE)
+tol = 1e-10;
+assert(normest(dsTE - ds(2*np+1:end, 2*np+1:end)) < tol, 'Ds matrices dont match')
+assert(normest(dst(1:2*np, 1:2*np) - dstTE) < tol, 'Dst matrices dont match')
+assert(normest(da(1:2*np, 1:2*np) - daTE) < tol, 'Da matrices dont match')
+assert(normest(dat(2*np+1:end, 2*np+1:end) - datTE) < tol, 'Dat matrices dont match')
