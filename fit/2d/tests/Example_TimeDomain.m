a5b575e2af6aa28401959d7f1343909c18b4456e
ChristophReinwald <christoph_juergen.reinwald@stud.tu-darmstadt.de>
2023-10-01 01:48:14 +0200

    Time domain and test skripts
    New time domain solver and examples for fd and td

--- fit/2d/tests/Example_TimeDomain.m
+++ fit/2d/tests/Example_TimeDomain.m
@@ -22,8 +22,8 @@ addpath(path_msh_func, path_mat_func, path_solver_func, path_util_func)
 %% Edit basic calculation domain
 
 % Steps of mesh
-xmesh = linspace(0,2,300);
-ymesh = linspace(0,1,200);
+xmesh = linspace(0,2,200);
+ymesh = linspace(0,1,100);
 
 % Create basic mesh object
 msh = cartMesh_2D(xmesh, ymesh); 
@@ -34,7 +34,7 @@ ny = msh.ny;
 np = msh.np;
 
 % Edit boundary conditions
-bc.bc = ["PMC", "OPEN", "PMC", "OPEN"];
+bc.bc = ["PMC", "OPEN", "PMC", "PEC"];
 
 
 %% Edit material regions and add them to the object material_regions
@@ -58,8 +58,13 @@ material_regions.boxesMuiR = boxesMuiR;
 
 %% Create Excitation
 
-% Frequency for harmonic excitation
-f = 1e9;
+% Harmonic 
+f = 5e8;
+
+% Time parameters
+dt = 1e-11;
+tend = 4.5/f;
+steps = ceil(tend/dt);
 
 % Create empty jsbow_space vector
 jsbow_excitation = NaN(3*np, 1);
@@ -68,11 +73,11 @@ jsbow_excitation = NaN(3*np, 1);
 e_exitation = NaN(3*np, 1);
 
 % Determine indices for electric field excitation
-x_L = ceil(msh.nx/2);
+x_L = ceil(msh.nx*3/4);
 n = 1 + (x_L-1)*Mx + ((1:ny)-1)*My + 2*np; 
 
-% Set corresponding entries in e_exi to the value of the desired amplitude
-% -> Use corresponding edges as source
+% Set corresponding entries in e_exi to one, so these edges can be used as
+% sources
 e_exitation(n) = 1;  
 
 
@@ -86,18 +91,60 @@ e_exitation(n) = 1;
 [MAT, bc] = generate_MAT(msh, bc, material_regions, ["CurlP"]); %#ok<NBRAK2> 
 
 
-%% Simulate
+%% Initialize simulation in time domain with leapfrog
 
-[ebow, hbow] = solve_helmholtz_2d_fd(msh, W, MAT.c, MAT.meps, MAT.mmui, jsbow, e_exi, f, bc);
+% Function for excitation
+s_source = e_exi;
+e_harm = @(t)(s_source * 1 * sin(2*pi*f*t));
 
+% Initialize open boundary condition if needed
+[mur_edges,mur_n_edges, mur_deltas] = initMur_2D(msh, bc);
 
-%% Plot solution for electric field in z-direction
+% Initialize ebow and hbow
+ebow_new = sparse(3*np,1);
+hbow_new = sparse(3*np,1);
 
+% Add inverse permittivity matrix
+MAT.mepsi = nullInv(MAT.meps);
+
+% Plot parameter for "movie"
 figure(1)
-idx2plot = 2*np+1:3*np;
-[X,Y] = meshgrid(msh.xmesh, msh.ymesh);
-e_surf = reshape(real(ebow(idx2plot)), [msh.nx, msh.ny]);
-e_surf_plot = surf(X,Y,e_surf');
-set(e_surf_plot,'LineStyle','none')
-set(gca,'ColorScale','log')
-drawnow
+zlimit = 2.5;
+draw_only_every = 10;
+
+
+%% Execute simulation by applying Leapfrog algorithm
+for ii = 0:steps-1
+
+    % Calculate time t
+    t = ii*dt;
+
+    % Old values
+    ebow_old = ebow_new;
+    hbow_old = hbow_new;
+
+    % Calculate value for excitation
+    e_exi_old = e_harm(t);
+    e_exi_new = e_harm(t+1);
+
+    % Execute timestep with leapfrog
+    [ebow_new,hbow_new] = solve_leapfrog_2d_td(ebow_old,hbow_old,e_exi_old,e_exi_new,jsbow,MAT.mmui,MAT.mepsi,MAT.c,dt,W);
+
+    % Apply open boundary with mur cond
+    ebow_new = applyMur_2D(mur_edges, mur_n_edges, mur_deltas, ebow_old, ebow_new, dt, bc);
+
+    % Draw electric field
+    idx2plot = 2*np+1:3*np;
+    [X,Y] = meshgrid(msh.xmesh, msh.ymesh);
+    if mod(ii, draw_only_every)
+        f = figure(1);
+        e_surf = reshape(ebow_new(idx2plot), [msh.nx, msh.ny]);
+        e_surf_plot = surf(X,Y,e_surf');
+        zlim([-zlimit zlimit])
+        set(e_surf_plot,'LineStyle','none')
+        set(gca,'ColorScale','log')
+        drawnow
+
+    end
+
+end
