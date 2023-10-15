78ff4cdb64f6083c524e7f10b8f0544aebef5bbd
ChristophReinwald <christoph_juergen.reinwald@stud.tu-darmstadt.de>
2023-10-16 01:03:15 +0200

    Update documentation

--- fit/2d/util/nullInv.m
+++ fit/2d/util/nullInv.m
@@ -1,23 +1,18 @@
-
 function AInv = nullInv(A)
-%% Description
-%
-% Function to determine the pseudo inverse to a square diagonal matrix by
+% nullInv determines the pseudo inverse to a square diagonal matrix by
 % inverting each element not equal to zero
 %
 % Input
-% A                 square diagonal matrix to invert 
+% A                 -square diagonal matrix to invert 
 %
 % Output
-% AInv              pseudo inverse of A (square diagonal)
+% AInv              -pseudo inverse of A (square diagonal)
 
 
-%% Function definition
+% Find indices and elements unequal to zero
+[indm, indn, values] = find(A);
 
-    % Find indices and elements unequal to zero
-    [indm, indn, values] = find(A);
-
-    % Calculate pseudo inverse
-    AInv = sparse(indm, indn, 1./values, size(A,1), size(A,2));
+% Calculate pseudo inverse
+AInv = sparse(indm, indn, 1./values, size(A,1), size(A,2));
 
 end
