2e3803569864fe66610e1a4e29e73ca0ec222a13
jannisTUD <133676800+jannisTUD@users.noreply.github.com>
2023-10-24 09:11:22 +0200

    Aufteilung Frequenzberechnung

--- task1/2d_td/main.m
+++ task1/2d_td/main.m
@@ -106,9 +106,9 @@ ebow_bc = NaN(3*msh.np, 1);
 ebow_bc(idx_bc) = 1;
 
 
-%% Solve Helmholtz
 
-% Apply bc and create matrices
+
+%% Apply bc and create matrices
 [bc, W, ebow_bc, jsbow] = apply_bc(msh, bc, ebow_bc, jsbow_bc);
 [MAT, bc] = generate_MAT(msh, bc, material_regions, ["CurlP"]);
 MAT.mepsi = nullInv(MAT.meps);
@@ -120,22 +120,25 @@ tend = 30/f1;
 nt = ceil(tend/dt);
 
 % Excitation
-ebow_exi = @(t) ebow_bc * (E1*cos(2*pi*f1*t) + E2*cos(2*pi*f2*t));
+ebow_exi_f1 = @(t) ebow_bc * (E1*cos(2*pi*f1*t));
+ebow_exi_f2 = @(t) ebow_bc * (E2*cos(2*pi*f2*t));
 
-% Vectors
+
+%% Solve for f1 
+% init Vectors
 ebow = zeros(3*msh.np, nt);
 hbow = zeros(3*msh.np, nt);
 ebow_abs = zeros(msh.np, nt);
 
-% Solve
+% Solve with leapfrog
 for i = 1:nt
     t = (i-1)*dt;
 
     % Save old and new values
     ebow_old = ebow(:,i);
     hbow_old = hbow(:,i);
-    e_exi_old = ebow_exi(t);
-    e_exi_new = ebow_exi(t+1);
+    e_exi_old = ebow_exi_f1(t);
+    e_exi_new = ebow_exi_f1(t+1);
 
     % Calc leapfrog
     [ebow_new, hbow_new] = solve_leapfrog_2d_td(ebow_old,hbow_old,e_exi_old,e_exi_new,jsbow,MAT.mmui,MAT.mepsi,MAT.c,dt,W);
@@ -146,11 +149,60 @@ for i = 1:nt
     % Save ebow and hbow
     ebow(:,i+1) = ebow_new;
     hbow(:,i+1) = hbow_new;
-    ebow_abs(:,i+1) = calc_abs_field(msh,ebow_new);
+    ebow_abs_f1(:,i+1) = calc_abs_field(msh,ebow_new);
 
     if plot_field & mod(i, 10)
         [X,Y] = meshgrid(msh.xmesh, msh.ymesh);
-        e_surf = reshape(ebow_abs(:,i+1), [msh.nx, msh.ny]);
+        e_surf = reshape(ebow_abs_f1(:,i+1), [msh.nx, msh.ny]);
+
+        figure(1)
+        e_surf_plot = surf(X,Y,e_surf');
+
+        xlim([0, L])
+        ylim([-h/2, h/2])
+        set(e_surf_plot,'LineStyle','none')
+        view(2)
+        colormap hot;
+        title('Absolute value of electric field','Interpreter','latex')
+        xlabel('$x$ (m)','Interpreter','latex')
+        ylabel('$y$ (m)','Interpreter','latex')
+        zlabel('Absolute value','Interpreter','latex')
+        drawnow
+    end
+
+end
+
+%% Solve for f2
+
+% init Vectors
+ebow = zeros(3*msh.np, nt);
+hbow = zeros(3*msh.np, nt);
+ebow_abs_f2 = zeros(msh.np, nt);
+
+% Solve with leapfrog
+for i = 1:nt
+    t = (i-1)*dt;
+
+    % Save old and new values
+    ebow_old = ebow(:,i);
+    hbow_old = hbow(:,i);
+    e_exi_old = ebow_exi_f2(t);
+    e_exi_new = ebow_exi_f2(t+1);
+
+    % Calc leapfrog
+    [ebow_new, hbow_new] = solve_leapfrog_2d_td(ebow_old,hbow_old,e_exi_old,e_exi_new,jsbow,MAT.mmui,MAT.mepsi,MAT.c,dt,W);
+
+    % Apply open boundary with mur cond
+    ebow_new = applyMur_2D(mur_edges, mur_n_edges, mur_deltas, ebow_old, ebow_new, dt, bc);
+
+    % Save ebow and hbow
+    ebow(:,i+1) = ebow_new;
+    hbow(:,i+1) = hbow_new;
+    ebow_abs_f2(:,i+1) = calc_abs_field(msh,ebow_new);
+
+    if plot_field & mod(i, 10)
+        [X,Y] = meshgrid(msh.xmesh, msh.ymesh);
+        e_surf = reshape(ebow_abs_f2(:,i+1), [msh.nx, msh.ny]);
 
         figure(1)
         e_surf_plot = surf(X,Y,e_surf');
@@ -170,6 +222,37 @@ for i = 1:nt
 end
 
 %% Postprocessing
+
+% sum ebow after simulation
+ebow_abs = ebow_abs_f1 + ebow_abs_f2;
+
+[~,y] = calc_intensity(msh, ebow_abs, offset);
+
+% componentwise analytically comparison
+I1_farfield = intensity_farfield(E1, lambda1, d, delta, L, y);
+I1_farfield = I1_farfield / max(I1_farfield);
+I2_farfield = intensity_farfield(E2, lambda2, d, delta, L, y);
+I2_farfield = I2_farfield / max(I2_farfield);
+
+figure
+plot(y, I1_farfield, 'DisplayName', 'Farfiel f1', 'color', '#3d00ff')
+hold on
+plot(y, I2_farfield, 'DisplayName', 'Farfiel f2', 'color', '#3d00ff')
+% hold on
+%scatter(bright, zeros(max(size(bright)),1), "green", 'filled');
+% hold on;
+% scatter(dark, zeros(max(size(dark)),1), "black", 'filled');
+title('Analytical solution (farfield) at the screen at $x=L=10^6$m','Interpreter','latex')
+xlabel('Position at the screen $y$ (m)','Interpreter','latex')
+ylabel('Intensity $I$','Interpreter','latex')
+xlim([-h/2, h/2])
+legend()
+
+I1_helmholtz = intensity_helmholtz(E1, lambda1, d, delta, L, y, ceil(length(idx_bc)/2));
+I2_helmholtz = intensity_helmholtz(E2, lambda2, d, delta, L, y, ceil(length(idx_bc)/2));
+
+% sum ebow after simulation
+ebow_abs = ebow_abs_f1 + ebow_abs_f2;
 
 % Intensity calculation
 [I,y] = calc_intensity(msh, ebow_abs, offset);
